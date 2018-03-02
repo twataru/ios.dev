d176c60a425c1a89add0bb30492abc3d2939767c
taniuchi <w-taniuchi@crdoti.co.jp>
2018-03-02 04:08:45 +0000

    feat: add Question 03

--- src/Chapter01.swift
+++ src/Chapter01.swift
@@ -1,3 +1,6 @@
+/*
+  第1章: 準備運動
+*/
 class Chapter01{
   /*
     初期化処理
@@ -28,6 +31,36 @@ class Chapter01{
     print("# 01.「パタトクカシーー」")
     print(str[0] + str[2] + str[4] + str[6])
   }
+  
+  /*
+    # 02.「パトカー」＋「タクシー」＝「パタトクカシーー」
+    
+    「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
+  */
+  
+  class func Q02(){
+    let patcar = "パトカー"
+    let taxi = "タクシー"
+    
+    print("# 02.「パトカー」＋「タクシー」＝「パタトクカシーー」")
+    print(zip(patcar,taxi).map{String($0) + String($1)}.reduce("",+))
+  }
+  
+  /*
+    # 03.円周率
+    
+    "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，
+    各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+  */
+  class func Q03(){
+    let pai = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+              .split(separator: " ")
+              .map{String($0).prefix(1)}
+    print("# 03.円周率")
+    print(pai)
+  }
 }
 Chapter01.Q00()
 Chapter01.Q01()
+Chapter01.Q02()
+Chapter01.Q03()
